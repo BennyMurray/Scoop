15b974a11c95b527d086fdefa85287ecd0727b07
Benjamin Murray <bjamurray@gmail.com>
2017-01-03 22:55:57 +0000

    Updated Admin

--- ScoopApp/admin.py
+++ ScoopApp/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from .models import Beer
+from .models import CraftBeer
+from django.utils.html import format_html
 
-admin.site.register(Beer)
+
+
+class CraftBeerAdmin(admin.ModelAdmin):
+
+    def image_tag(self, obj):
+        return format_html('<img src="{}" />'.format(obj.image_link))
+
+    image_tag.short_description = 'Image'
+
+    list_display = ['beer_name', 'image_tag']
+
+admin.site.register(CraftBeer, CraftBeerAdmin)
